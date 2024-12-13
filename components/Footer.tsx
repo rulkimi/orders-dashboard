61d985a73da0ef8165e7506ca9aba24fcc8d6bd1
rulkimi <azrulkimi00@gmail.com>
2024-12-13 23:26:25 +0800

    footer qbeep link to go to qbeep page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="fixed bottom-0 flex justify-center w-full p-6 border-t">
       <div>
-        Copyright 2023 <span className="text-blue-500">Qbeep</span> <span className="text-slate-600">| All right reserved | April 4,2024 3:01 PM</span>
+        Copyright 2023 <a href="https://qbeep.com/" target="_blank" className="text-blue-500">Qbeep</a> <span className="text-slate-600">| All right reserved | April 4,2024 3:01 PM</span>
       </div>
     </div>
   )

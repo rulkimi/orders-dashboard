692c36536411a667df6698ae5388f301250da6e6
rulkimi <azrulkimi00@gmail.com>
2024-12-16 23:27:40 +0800

    fix fake chart not rendered height properly

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,15 @@
-import DashboardCard from "@/components/DashboardCard"
-import { DashboardCardItem } from "@/components/DashboardCard"
+"use client"
+
+import { useState, useEffect } from "react";
+import DashboardCard from "@/components/DashboardCard";
+import { DashboardCardItem } from "@/components/DashboardCard";
 import { Order, columns } from "@/app/orders/columns";
 import { DataTable } from "@/app/orders/data-table";
 import { getData } from "../orders/page";
 import FakeChart from "./fake-chart";
 
 async function getInfo(): Promise<DashboardCardItem[]> {
-  const response = await fetch('http://localhost:8000/info')
+  const response = await fetch('http://localhost:8000/info');
 
   if (!response.ok) {
     throw new Error('Failed to fetch data');
@@ -16,10 +19,31 @@ async function getInfo(): Promise<DashboardCardItem[]> {
   return info;
 }
 
-export default async function Dashboard() {
-  const dashboardCardItems = await getInfo();
-  const tableData = await getData();
-  
+export default function Dashboard() {
+  const [dashboardCardItems, setDashboardCardItems] = useState<DashboardCardItem[]>([]);
+  const [tableData, setTableData] = useState<Order[]>([]);
+  const [isDataUpdated, setIsDataUpdated] = useState<boolean>(false); 
+  const [renderFakeChart, setRenderFakeChart] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const dashboardItems = await getInfo();
+        const orders = await getData();
+        
+        setDashboardCardItems(dashboardItems);
+        setTableData(orders);
+        setIsDataUpdated(true); 
+        setTimeout(() => {
+          setRenderFakeChart(true)
+        }, 50);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <ul className="grid grid-cols-12 gap-4 mb-4">
@@ -31,12 +55,12 @@ export default async function Dashboard() {
       </ul>
       <div className="flex gap-4">
         <div className="w-1/2">
-          <FakeChart />
+          {(isDataUpdated && renderFakeChart) && <FakeChart />}
         </div>
         <div className="w-1/2">
-          <DataTable columns={columns} data={tableData} isDashboard />
+          {isDataUpdated && <DataTable columns={columns} data={tableData} isDashboard />}
         </div>
       </div>
     </div>
-  )
+  );
 }

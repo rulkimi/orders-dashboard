e9a436e280721331b18ee8cbb689b88da5f92a17
rulkimi <azrulkimi00@gmail.com>
2024-12-19 09:41:23 +0800

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/kimi-logo.svg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen antialiased`}
       >
